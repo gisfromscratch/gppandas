d62dd14e9136ed969f3c8d4a4c3cd0ecf85194fa
J.T <gisfromscratch@live.de>
2015-04-15 00:55:24 +0200

    Added the CSV OSM data and the initial implementation

---
+++ PandasDemos/PandasDemos/PandasDemos.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+def valueToFloat(value):
+    """Converts the given value as float.
+    value : the value which should be treated as a float
+    """
+    if (str('') == str(value).strip()):
+        return np.NaN;
+    try:
+        return float(value.replace(',', '.'))
+    except:
+        return np.NaN 
+
+# Create a series
+pointTypes = pd.Series(['bus_stop', 'kiosk', 'traffic_signals'])
+print unicode(pointTypes)
+print
+
+# Read the data, ignore errors, decimal=',' only works for the first coordinate
+coordinateConverters = {'XCoord': lambda coordinate: valueToFloat(coordinate), 'YCoord': lambda coordinate: valueToFloat(coordinate)}
+pointsData = pd.read_csv('C:/Data/OSM/ColognePoints.csv', converters=coordinateConverters, sep=';', encoding='utf-8', error_bad_lines=False)
+print unicode(pointsData.head())
+print
+
+# Describe the data
+print 'Columns and statistics'
+print unicode(pointsData.columns)
+print
+print unicode(pointsData.describe())
+print
+
+# Show null mask
+print unicode(pd.isnull(pointsData.head()))
+print
