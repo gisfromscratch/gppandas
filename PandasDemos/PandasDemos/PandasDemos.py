f83ee6aca5d95e48322b43a25c1405cb0db2dcb2
J.T <gisfromscratch@live.de>
2017-01-21 18:42:43 +0100

    Modified the sample for Python 3.4

--- PandasDemos/PandasDemos/PandasDemos.py
+++ PandasDemos/PandasDemos/PandasDemos.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import os
+#import matplotlib as plt
 
 def valueToFloat(value):
     """Converts the given value as float.
@@ -15,22 +16,22 @@ def valueToFloat(value):
 
 # Create a series
 pointTypes = pd.Series(['bus_stop', 'kiosk', 'traffic_signals'])
-print unicode(pointTypes)
+#print unicode(pointTypes)
 print
 
 # Read the data, ignore errors, decimal=',' only works for the first coordinate
 coordinateConverters = {'XCoord': lambda coordinate: valueToFloat(coordinate), 'YCoord': lambda coordinate: valueToFloat(coordinate)}
-pointsData = pd.read_csv('C:/Data/OSM/ColognePoints.csv', converters=coordinateConverters, sep=';', encoding='utf-8', error_bad_lines=False)
-print unicode(pointsData.head())
+pointsData = pd.read_csv(os.path.abspath('../../Data/ColognePoints.csv'), converters=coordinateConverters, sep=';', encoding='utf-8', error_bad_lines=False)
+#print unicode(pointsData.head())
 print
 
 # Describe the data
-print 'Columns and statistics'
-print unicode(pointsData.columns)
+print ('Columns and statistics')
+#print unicode(pointsData.columns)
 print
-print unicode(pointsData.describe())
+#print unicode(pointsData.describe())
 print
 
 # Group by TYPE
-print unicode(pointsData.groupby('TYPE').size())
+#print unicode(pointsData.groupby('TYPE').size())
 print
