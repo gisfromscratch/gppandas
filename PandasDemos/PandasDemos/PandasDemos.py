760329199867b71f04c9235248a6e9fc0fa3dd02
J.T <gisfromscratch@live.de>
2017-01-23 00:03:01 +0100

    Inserting point geometries using arcpy

--- PandasDemos/PandasDemos/PandasDemos.py
+++ PandasDemos/PandasDemos/PandasDemos.py
@@ -1,3 +1,5 @@
+import arcview
+import arcpy
 import pandas as pd
 import numpy as np
 import os
@@ -14,10 +16,10 @@ def valueToFloat(value):
     except:
         return np.NaN 
 
-# Create a series
-pointTypes = pd.Series(['bus_stop', 'kiosk', 'traffic_signals'])
-print(pointTypes)
-print
+## Create a series
+#pointTypes = pd.Series(['bus_stop', 'kiosk', 'traffic_signals'])
+#print(pointTypes)
+#print
 
 # Read the data, ignore errors, decimal=',' only works for the first coordinate
 coordinateConverters = {'Longitude': lambda coordinate: valueToFloat(coordinate), 'Latitude': lambda coordinate: valueToFloat(coordinate)}
@@ -26,16 +28,41 @@ colNames = ['GeonameId','Name','ASCIIName','AlternateNames',
 'CountryCode','AlternateCountryCodes','Admin1Code','Admin2Code',
 'Admin3Code','Admin4Code','Population','Elevation','DEM','Timezone','ModificationDate']
 pointsData = pd.read_csv(os.path.abspath('../../Data/allCountries_extract.txt'), header=None, names=colNames, converters=coordinateConverters, sep='\t', encoding='utf-8', error_bad_lines=False)
-print(pointsData.head())
-print
+#print(pointsData.head())
+#print
 
-# Describe the data
-print('Columns and statistics')
-print(pointsData.columns)
-print
-print(pointsData.describe())
-print
+## Describe the data
+#print('Columns and statistics')
+#print(pointsData.columns)
+#print
+#print(pointsData.describe())
+#print
 
-# Group by TYPE
-print(pointsData.groupby('CountryCode').size())
-print
+## Group by TYPE
+#print(pointsData.groupby('CountryCode').size())
+#print
+
+# Create a local geodatabase
+workDir = os.path.abspath('')
+gdbName = 'Geonames.gdb'
+gdbPath = os.path.join(workDir, gdbName)
+if (os.path.isdir(gdbPath)) :
+    arcpy.Delete_management(gdbPath)
+arcpy.CreateFileGDB_management(workDir, gdbName)
+
+# Create feature class
+tableName = 'points'
+spatialRef = arcpy.SpatialReference(4326)
+arcpy.CreateFeatureclass_management(gdbPath, tableName, 'POINT', None, 'DISABLED', 'DISABLED', spatialRef)
+
+# Insert features
+connectionPath = os.path.join(gdbPath, tableName)
+insertCursor = arcpy.da.InsertCursor(connectionPath, ('SHAPE@XY'))
+pointBuffer = arcpy.Point()
+for record in pointsData.itertuples():
+    lon = record[6]
+    lat = record[5]
+    pointBuffer.X = lon
+    pointBuffer.Y = lat
+    insertCursor.insertRow([pointBuffer])
+del insertCursor
