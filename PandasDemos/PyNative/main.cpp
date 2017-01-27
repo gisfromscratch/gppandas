352e76cf00ba14bb68ef0600ec6e81ae9d9e65cf
J.T <gisfromscratch@live.de>
2017-01-27 23:57:57 +0100

    Simple double add implementation

--- PandasDemos/PyNative/main.cpp
+++ PandasDemos/PyNative/main.cpp
@@ -1,1 +1,34 @@
 #include "PyNative.h"
+
+PyObject* add(PyObject *self, PyObject *args)
+{
+	// dd means two doubles
+	double x, y;
+	if (!PyArg_ParseTuple(args, "dd", &x, &y))
+	{
+		return 0;
+	}
+
+	return PyFloat_FromDouble(x+y);
+}
+
+static struct PyMethodDef pyNativeMethodsDef[]
+{
+	{"add", (PyCFunction) add, METH_VARARGS, NULL },
+	{ NULL, NULL, 0, NULL } /* Sentinel */
+};
+
+static struct PyModuleDef pyNativeModuleDef
+{
+	PyModuleDef_HEAD_INIT,
+	"PyNative",
+	"",
+	-1,
+	pyNativeMethodsDef
+};
+
+PyMODINIT_FUNC
+PyInit_PyNative(void)
+{
+	return PyModule_Create(&pyNativeModuleDef);
+}
